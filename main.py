050c26b77a0cfc83d2f2d642d9ea61421bc03aed
arodrica43 <lexws33@gmail.com>
2025-07-05 12:30:38 +0200

    Update main.py
    Adding mongo DB connection

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+import os
+
+uri = os.getenv("MONGO_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    try:
+        client.admin.command('ping')
+        return {"message": "Pinged your deployment. You successfully connected to MongoDB!"}
+    except Exception as e:
+        return {"message": e}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
